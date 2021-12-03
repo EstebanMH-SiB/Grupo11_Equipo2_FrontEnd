a83b4d0782e47d8fd689fd71e1b8bf8638688a04
EstebanMHMisionTic <emarentes@uninorte.edu.co>
2021-12-03 00:33:37 -0500

    Agregar el componente Registrar Usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './bootstrap.css';
 import NavBar from './components/NavBar.js';
 import Footer from './components/Footer.js';
+import RegistroUsuario from './components/RegistroUsuario';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-
+      <RegistroUsuario />
       </header>
       <Footer />
     </div>
